717337f8fb5b3c491a9cb15f673f1521f2ba3bf0
lakhan raut <lakhanraut24@gmail.com>
2022-08-07 17:29:26 +0530

    cart span change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -83,7 +83,7 @@ const Navbar = () => {
             <div style={cartstyle}>
                
                 <img className="ml-2  " src="/Images/cart.png" alt="cart-icon" />
-                <span className="pt-2 font-bold"> 5</span>
+                <span className="pt-2 font-bold"> cart(0)</span>
                
               </div>
             </Link>
